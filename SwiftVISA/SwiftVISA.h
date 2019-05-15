622337a88c280099b722e9777c9f57a35e278ae8
Field Session MacBook <ohildreth@mines.edu>
2019-05-14 20:07:07 -0600

    Initial Commit

---
+++ SwiftVISA/SwiftVISA.h
@@ -0,0 +1,19 @@
+//
+//  SwiftVISA.h
+//  SwiftVISA
+//
+//  Created by Avinash on 5/14/19.
+//  Copyright © 2019 SwiftVISA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftVISA.
+FOUNDATION_EXPORT double SwiftVISAVersionNumber;
+
+//! Project version string for SwiftVISA.
+FOUNDATION_EXPORT const unsigned char SwiftVISAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftVISA/PublicHeader.h>
+
+
