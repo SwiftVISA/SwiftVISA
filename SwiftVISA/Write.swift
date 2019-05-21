7e7e9a4edf2bee840cafb195e749e66baaa40edf
seeaya <connorbarnesdenver@icloud.com>
2019-05-21 13:15:59 -0600

    Fixed writing to not use encoders.

--- SwiftVISA/Write.swift
+++ SwiftVISA/Write.swift
@@ -13,17 +13,11 @@ import CVISA
 ///   - instrument: The instrument to write to.
 ///   - string: The string to write.
 /// - Throws: If the data could not be properly written.
-public func visaWrite<T, E: VISAEncoder>(to instrument: Instrument, _ value: T, encoder: E) throws where E.EncodingType == T {
-	let string = try encoder.encode(value)
+public func visaWrite(to instrument: Instrument, _ string: String) throws {
 	var returnCount = ViUInt32()
 	let status = viWrite(instrument.session, string, ViUInt32(string.count), &returnCount)
 	
 	if status < VI_SUCCESS {
 		throw WriteError(status) ?? UnknownError()
 	}
-}
-
-public func visaWrite<T: VISAEncodable>(to instrument: Instrument, _ value: T) throws {
-	let encoder = T.DefaultVISAEncoder()
-	try visaWrite(to: instrument, value, encoder: encoder)
 }
