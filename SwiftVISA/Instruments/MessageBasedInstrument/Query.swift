f2562deb0b2e1e61e0fc062faf4bf7883d54a782
Luke H <luop90@gmx.com>
2019-05-31 16:12:07 -0600

    Added query multiple

--- SwiftVISA/Instruments/MessageBasedInstrument/Query.swift
+++ SwiftVISA/Instruments/MessageBasedInstrument/Query.swift
@@ -67,4 +67,65 @@ extension MessageBasedInstrument {
 		try write(message)
 		return try read(as: T.self)
 	}
+
+	/// Writes the given message to the instrument and then reads multiple data points from the given instrument and decodes it to the given type using the default decoder.
+	///
+	/// - Parameters:
+	///   - message: The message to write to the instrument.
+	///   - type: The type to return.
+	/// - Returns: The decoded value.
+	/// - Throws: One of the following `VISAError` errors:
+	///   - `.invalidSession`
+	///   - `.unsupportedOperations`
+	///   - `.resourceLocked`
+	///   - `.timeout`
+	///   - `.rawWriteProtocolViolation`
+	///   - `.rawReadProtocolViolation`
+	///   - `inputProtocolViolation`
+	///   - `.outputProtocolViolation`
+	///   - `.busError`
+	///   - `.invalidSetup`
+	///   - `.notControllerInCharge`
+	///   - `.noListeners`
+	///   - `.parityError`
+	///   - `.framingError`
+	///   - `.overrunError`
+	///   - `.ioError`
+	///   - `.connectionLost`
+	///   - `.couldNotDecode`
+	public func query<T: VISADecodable>(_ message: String, as type: T.Type, numberOfReads: Int, timeBetweenReads: TimeInterval = 0.025) throws -> [T?] {
+		try write(message)
+		return try read(as: T.self, numberOfReads: numberOfReads, timeBetweenReads: timeBetweenReads)
+	}
+
+	/// Writes the given message to the instrument and then reads data from the given instrument and decodes it to the given type using the given decoder.
+	///
+	/// - Parameters:
+	///   - message: The message to write to the instrument.
+	///   - type: The type to return.
+	///   - decoder: The decoder to use to decode the data.
+	/// - Returns: The decoded value.
+	/// - Throws: One of the following `VISAError` errors:
+	///   - `.invalidSession`
+	///   - `.unsupportedOperations`
+	///   - `.resourceLocked`
+	///   - `.timeout`
+	///   - `.rawWriteProtocolViolation`
+	///   - `.rawReadProtocolViolation`
+	///   - `inputProtocolViolation`
+	///   - `.outputProtocolViolation`
+	///   - `.busError`
+	///   - `.invalidSetup`
+	///   - `.notControllerInCharge`
+	///   - `.noListeners`
+	///   - `.parityError`
+	///   - `.framingError`
+	///   - `.overrunError`
+	///   - `.ioError`
+	///   - `.connectionLost`
+	///   - `.couldNotDecode`
+	public func query<T, D: VISADecoder>(_ message: String, as type: T.Type, decoder: D, numberOfReads: Int, timeBetweenReads: TimeInterval = 0.025) throws -> [T?] where D.DecodingType == T {
+		try write(message)
+		return try read(as: T.self, decoder: decoder, numberOfReads: numberOfReads, timeBetweenReads: timeBetweenReads)
+	}
 }
