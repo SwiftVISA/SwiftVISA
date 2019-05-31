8cd7236e93da5272581c8f34c02568082b82010a
Luke H <luop90@gmx.com>
2019-05-31 15:49:53 -0600

    Refactored ReadMultiple -> read and moved into read file, fully implemented ReadMultiple(read).

--- SwiftVISA/Instruments/MessageBasedInstrument/Read.swift
+++ SwiftVISA/Instruments/MessageBasedInstrument/Read.swift
@@ -105,4 +105,146 @@ extension MessageBasedInstrument {
 		let visaString = try read()
 		return try T(visaString: visaString)
 	}
+
+	// TODO: Why is the default time delay 25 ms? Should there be any default? There probably shouldn't be a default value unless the value has significant meaning and/or will be used in a large percentage of calls.
+	// TODO: Should the function allow a value of 0 for timeDelay that indicates that the reads should be performed immediately after one another? This would result in an array with readings from unknown time intervals, but may be useful for reading data as fast as possible. If this was allowed, maybe 0 should be the default value?
+	// TODO: Should this method signiture be changed? The function name is not very "swifty". The first argument label does not read as English, and the timeDelay argument label is confusing: is this a delay before reading, after, during, etc.? Perhaps a better signiture would be "read(as:numberOfTimes:timeInterval)" here however, the timeInterval argument label may be similarly vague. The name "read(as:numberOfTimes:timeStep)" or "read(as:numberOfTimes:timeBetweenReads)" could also work, however the first reads oddly to me, and the second is fairly long. Another benefit to any three of these names is that the signature would match the read(as:) method but with additional parameters. This would help aid homogeneity in the code.
+	/// Repeatedly reads values from an instrument with the specified amount of time between each read and returns an array of these values.
+	///
+	/// This function performs `numberOfReads` number of reads sequentially and returns the results from the read as an array. Each reading is taken with `timeDelay` number of seconds between them. Each non-nil sequential value in the array is guarenteed to have occurred with `timeDelay` number of seconds between them. If the instrument's read could not be completed in `timeDelay` number of seconds, then the next value(s) in the array are `nil`, indicating that this time index was skipped. Further, the resuling array will always have `numberOfReads` elements.
+	///
+	/// **Example:**
+	///
+	/// The following call is made:
+	///
+	///     readMultiple(numberOfReads: 5, as: Int.self, timeDelay: 0.5)
+	///
+	/// The first read takes 0.4 seconds, and returns a `3`. The computer waits 0.1 seconds before making the next read. The next read takes 0.3 seconds, and returns a `5`. The computer waits 0.2 seconds before making the next read. The next read takes 0.6 seconds, and returns a `6`. Because the next read was scheduled to happen 0.1 seconds ago, the read is skipped, and `nil` is used for this time index. The last read is made and takes 0.8 seconds and returns `5`. Although the read operation took longer than the time delay, there are no further scheduled read operations, so an additional `nil` is not used.
+	///
+	/// The resulting array is returned:
+	///
+	///     [3, 5, 6, nil, 5]
+	///
+	/// - Parameters:
+	///   - numberOfReads: The number of reads to perform.
+	///   - type: The type to be returned from each read.
+	///   - timeDelay: The amount of time between each successive read operation, defaults to 25 ms.
+	/// - Returns: An array of the results of the successive read operations.
+	/// - Throws: One of the following `VISAError` errors:
+	///   - `.invalidSession`
+	///   - `.unsupportedOperations`
+	///   - `.resourceLocked`
+	///   - `.timeout`
+	///   - `.rawWriteProtocolViolation`
+	///   - `.rawReadProtocolViolation`
+	///   - `.outputProtocolViolation`
+	///   - `.busError`
+	///   - `.invalidSetup`
+	///   - `.notControllerInCharge`
+	///   - `.noListeners`
+	///   - `.parityError`
+	///   - `.framingError`
+	///   - `.overrunError`
+	///   - `.ioError`
+	///   - `.connectionLost`
+	///   - `.couldNotDecode`
+	public func read<T: VISADecodable>(as type: T, numberOfReads: Int, timeBetweenReads: TimeInterval = 0.025) throws -> [T?] {
+		#warning("Not tested")
+		var readList: [T?] = []
+
+		for var i in 1...numberOfReads {
+			let startTime = Date()
+			var nextRead: T?
+
+			do {
+				nextRead = try self.read(as: T.self)
+			} catch {
+				nextRead = nil
+			}
+
+			readList.append(nextRead)
+			let endTime = Date()
+			let timeElapsed = endTime.timeIntervalSince(startTime)
+
+			// Calculation of the time delay before we call read again
+			let currentTimeDelay = timeBetweenReads - timeElapsed
+
+			if (currentTimeDelay < 0) {
+				usleep(UInt32(currentTimeDelay * 1000000.0))
+			} else {
+				// If the read took longer than the time elapsed, than skip the next one and insert nill
+				readList.append(nil)
+				i += 1
+			}
+		}
+
+		return readList
+	}
+
+	// TODO: Why is the default time delay 25 ms? Should there be any default? There probably shouldn't be a default value unless the value has significant meaning and/or will be used in a large percentage of calls.
+	// TODO: Should the function allow a value of 0 for timeDelay that indicates that the reads should be performed immediately after one another? This would result in an array with readings from unknown time intervals, but may be useful for reading data as fast as possible. If this was allowed, maybe 0 should be the default value?
+	// TODO: Should this method signiture be changed? The function name is not very "swifty". The first argument label does not read as English, and the timeDelay argument label is confusing: is this a delay before reading, after, during, etc.? Perhaps a better signiture would be "read(as:numberOfTimes:timeInterval)" here however, the timeInterval argument label may be similarly vague. The name "read(as:numberOfTimes:timeStep)" or "read(as:numberOfTimes:timeBetweenReads)" could also work, however the first reads oddly to me, and the second is fairly long. Another benefit to any three of these names is that the signature would match the read(as:) method but with additional parameters. This would help aid homogeneity in the code.
+	/// Repeatedly reads values from an instrument with the specified amount of time between each read and returns an array of these values.
+	///
+	/// This function performs `numberOfReads` number of reads sequentially and returns the results from the read as an array. Each reading is taken with `timeDelay` number of seconds between them. Each non-nil sequential value in the array is guarenteed to have occurred with `timeDelay` number of seconds between them. If the instrument's read could not be completed in `timeDelay` number of seconds, then the next value(s) in the array are `nil`, indicating that this time index was skipped. Further, the resuling array will always have `numberOfReads` elements.
+	///
+	/// The resulting array is returned:
+	///
+	///     [3, 5, 6, nil, 5]
+	///
+	/// - Parameters:
+	///   - numberOfReads: The number of reads to perform.
+	///   - type: The type to be returned from each read.
+	///   - decoder: The decoder to use to decode the data.
+	///   - timeDelay: The amount of time between each successive read operation, defaults to 25 ms.
+	/// - Returns: An array of the results of the successive read operations.
+	/// - Throws: One of the following `VISAError` errors:
+	///   - `.invalidSession`
+	///   - `.unsupportedOperations`
+	///   - `.resourceLocked`
+	///   - `.timeout`
+	///   - `.rawWriteProtocolViolation`
+	///   - `.rawReadProtocolViolation`
+	///   - `.outputProtocolViolation`
+	///   - `.busError`
+	///   - `.invalidSetup`
+	///   - `.notControllerInCharge`
+	///   - `.noListeners`
+	///   - `.parityError`
+	///   - `.framingError`
+	///   - `.overrunError`
+	///   - `.ioError`
+	///   - `.connectionLost`
+	///   - `.couldNotDecode`
+	public func read<T, D: VISADecoder>(s type: T, decoder: D, numberOfReads: Int, timeBetweenReads: TimeInterval = 0.025) throws -> [T?] where D.DecodingType == T {
+		#warning("Not tested")
+		var readList: [T?] = []
+
+		for var i in 1...numberOfReads {
+			let startTime = Date()
+			do {
+				readList.append(try self.read(as: T.self, decoder: decoder))
+			} catch {
+				// TODO: If the read throws an error, I believe a nil will be appended into the readList
+				// If not, simply append it here?
+			}
+
+			let endTime = Date()
+
+			let timeElapsed = endTime.timeIntervalSince(startTime)
+
+			// Calculation of the time delay before we call read again
+			let currentTimeDelay = timeBetweenReads - timeElapsed
+
+			if (currentTimeDelay < 0) {
+				usleep(UInt32(currentTimeDelay * 1000000.0))
+			} else {
+				// If the read took longer than the time elapsed, than skip the next one and insert nill
+				readList.append(nil)
+				i += 1
+			}
+		}
+
+		return readList
+	}
 }
