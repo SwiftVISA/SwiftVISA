5966bc6d3d2b72f4d336d1ab4119ab32b13086ee
seeaya <connorbarnesdenver@icloud.com>
2019-06-03 12:10:54 -0600

    Added warning for untested code.

--- SwiftVISA/Instruments/Attributes.swift
+++ SwiftVISA/Instruments/Attributes.swift
@@ -13,10 +13,36 @@ extension Instrument {
 	///
 	/// - Throws: ???
 	func setAttibute(_ attributeId: ViAttr, value: String) throws {
+		#warning("Unimlemented")
 		// TODO make sure that attribute responses are never greater than 2048 bytes
 		let buffer = ViPBuf.allocate(capacity: 2048)
 		
 		// let status = viSetAttribute(session.viSession, attributeId, value)
 		// guard status >= VI_SUCCESS else { throw VISAError(status)}
+	}
+	
+	/// Gets the given NI-VISA attribute on the instrument
+	///
+	/// - Throws: ???
+	func getAttribute(_ attributeId: ViAttr) throws -> String {
+		#warning("Not tested")
+		let buffer = ViPBuf.allocate(capacity: 2048)
+		let status = viGetAttribute(session.viSession, attributeId, buffer)
+		
+		guard status >= VI_SUCCESS else { throw VISAError(status) }
+		// convert the data to a readable string format
+		let pointer = UnsafeRawPointer(buffer)
+		let bytes = MemoryLayout<UInt8>.stride * 2048
+		let data = Data(bytes: pointer, count: bytes)
+		guard let string = String(data: data, encoding: .ascii) else {
+			throw VISAError.couldNotDecode
+		}
+		let startIndex = string.startIndex
+		guard let endIndex = string.firstIndex(of: "\0") else {
+			// TODO remove this print when "rigorous" testing done
+			print("Buffer size bad (null byte not found) D:")
+			throw VISAError.couldNotDecode
+		}
+		return String(string[startIndex..<endIndex])
 	}
 }
