9c91689bcff00afa0f4ae46f48c5e9968666539a
Luke H <luop90@gmx.com>
2019-06-10 15:32:22 -0600

    Added readRaw unit test

--- SwiftVISATests/MessageBasedInstrumentTests.swift
+++ SwiftVISATests/MessageBasedInstrumentTests.swift
@@ -46,11 +46,19 @@ class MessageBasedInstrumentTests : XCTestCase {
 	func testReadDCVoltage() {
 		try? multimeterInstrument?.write("MEASURE:SCALAR:VOLTAGE:DC?")
 		let voltage = try? multimeterInstrument?.read(as: Double.self)
-		print(voltage ?? "Nothing returned")
 
 		XCTAssertNotNil(voltage)
 	}
-	
+
+	// Test the readRaw function
+	func testReadRaw() {
+		try? multimeterInstrument?.write("MEASURE:SCALAR:VOLTAGE:DC?")
+		let voltage: String? = try? multimeterInstrument?.readRaw()
+
+		XCTAssertNotNil(voltage) // Verify we got a voltage
+		XCTAssert(voltage!.last == "\n" || voltage!.last == "\0") // Verify there is a termination character
+	}
+
 	// Test the functionality of the Query command by writing and reading a DC Voltage
 	func testQuery() {
         let voltage = try? multimeterInstrument?.query("MEASURE:SCALAR:VOLTAGE:DC?", as: Double.self)
@@ -71,19 +79,20 @@ class MessageBasedInstrumentTests : XCTestCase {
         XCTAssertNotNil(voltage)
         XCTAssertEqual(voltage?.count, 10)
 	}
-    
+
+	// Test getting attributes
     func testGetAttribute() {
         let manufacture = try? waveformGeneratorInstrument?.getAttribute(UInt32(VI_ATTR_MANF_NAME), as: String.self)
 
         XCTAssertNotNil(manufacture)
         XCTAssertEqual(manufacture, "Agilent Technologies")
     }
-    
+
+	// Test setting attributes
     func testSetAttribute() {
-        XCTAssertNoThrow(try waveformGeneratorInstrument?.setAttribute(UInt32(VI_ATTR_TMO_VALUE), value: 1))
+        XCTAssertNoThrow(try waveformGeneratorInstrument?.setAttribute(UInt32(VI_ATTR_TMO_VALUE), value: 3000))
         let timeout = try! waveformGeneratorInstrument?.getAttribute(UInt32(VI_ATTR_TMO_VALUE), as: Int.self)
 
-        print(timeout as! Int)
         XCTAssertNotNil(timeout)
         XCTAssertEqual(timeout, 3000)
     }
