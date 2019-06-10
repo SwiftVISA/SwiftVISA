2a5f57d80fc26bafee7fa71d83e4be48fe35f59c
seeaya <connorbarnesdenver@icloud.com>
2019-06-10 13:39:53 -0600

    Added tests for multithread.

--- SwiftVISATests/MultithreadingTests.swift
+++ SwiftVISATests/MultithreadingTests.swift
@@ -11,18 +11,39 @@ import XCTest
 import CVISA
 
 class MultithreadingTests: XCTestCase {
-	let waitTime = 10_000
-	
 	func testWaveform() {
 		guard let instrument = try? InstrumentManager.default?.makeInstrument(identifier: "USB0::0x0957::0x2607::MY52200879::INSTR") as? MessageBasedInstrument else {
 			XCTFail()
+			return
 		}
 		
 		try? instrument.write("SOURCE1:FUNCTION DC")
 		try? instrument.write("OUTPUT1 ON")
 		
+		var done = false
 		
-		try? instrument.write("SOURCE1:VOLTAGE:OFFSET 2.5")
+		instrument.dispatchQueue.async {
+			print("Starting Async")
+			
+			for _ in 0...10 {
+				print("Do Something")
+				try? instrument.write("SOURCE1:VOLTAGE:OFFSET 0.5")
+//				let firstValue = try? instrument.read(as: Double.self)
+//				print(firstValue ?? "nil")
+				usleep(1_000_000)
+				try? instrument.write("SOURCE1:VOLTAGE:OFFSET 1.0")
+//				let secondValue = try? instrument.read(as: Double.self)
+//				print(secondValue ?? "nil")
+				usleep(1_000_000)
+			}
+			
+			print("Ending Async")
+			done = true
+		}
+		
+		while !done {
+			usleep(1_000)
+		}
 		
 	}
 }
